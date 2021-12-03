0a92be22e61703273a637a7c5c3595efc68d7c10
Henrique SC <jocs4@hotmail.com>
2021-12-03 10:08:38 -0300

    inicio implementação rest

---
+++ appgp/appgp/appgp.Android/obj/Debug/100/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data Hash: E1E39007C95EE0F0 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1294
+	/* entry-length */
+	.long	259
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 335147
+	.include	"typemap.jm.inc"
---
+++ appgp/appgp/appgp.Android/obj/Debug/100/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data Hash: 73BD79F3840913D0 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1441
+	/* entry-length */
+	.long	259
+	/* value-offset */
+	.long	142
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 373220
+	.include	"typemap.mj.inc"
